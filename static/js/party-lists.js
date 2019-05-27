178e823ca281d5e15257d9704c9a5ed8625038e4
Ben Hur <792704-benhur07b@users.noreply.gitlab.com>
2019-05-27 08:03:56 +0800

    added party lists maps.

---
+++ static/js/party-lists.js
@@ -0,0 +1,134 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9yc2NoYWNoMDdiIiwiYSI6ImNqYjh1NDRwYjBiaGEzM251bWF0a3c1cnAifQ.FEHNJbpxcs_UxuRhKbGF6A';
+
+document.getElementById('candidate').onchange = function() {updateMap()};
+document.getElementById('data').onchange = function() {updateMap()};
+
+function updateMap() {
+    var candidate = document.getElementById('candidate').value
+    var data = document.getElementById('data').value
+    var field = candidate + ' - ' + data
+    map.setPaintProperty('provinces+ncr',
+        'fill-color',
+            styleField(),
+     );
+}
+
+function styleField() {
+    var candidate = document.getElementById('candidate').value
+    var data = document.getElementById('data').value
+    var field = candidate + ' - ' + data
+
+    if (data == 'PERCENTAGE') {
+        return [
+        'step',
+        ['get', field],
+            // '#e8e8e8', 1.5,
+            // '#a1d8d8', 3.0,
+            // '#5ac8c8', 4.5,
+            // '#8c96ba', 6.0,
+            // '#be64ac', 100,
+            '#fff5f0', 1.0,
+            '#fdbea5', 2.0,
+            '#fc7050', 10.0,
+            '#d42020', 25.0,
+            '#67000d', 100,
+            ['rgba', 255, 255, 255, 0]
+        ]
+    }
+    else if (data == 'PERCENT TOTAL VOTES') {
+        return [
+        'step',
+        ['get', field],
+            '#fff5f0', 1.0,
+            '#fdbea5', 2.5,
+            '#fc7050', 5.0,
+            '#d42020', 10.0,
+            '#67000d', 100,
+            ['rgba', 255, 255, 255, 0]
+        ]
+    }
+    else if (data == 'VOTES') {
+        return [
+        'step',
+        ['get', field],
+            '#fff5f0', 500,
+            '#fdbea5', 5000,
+            '#fc7050', 10000,
+            '#d42020', 50000,
+            '#67000d', 10000000,
+            ['rgba', 255, 255, 255, 0]
+        ]
+    }
+}
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/rorschach07b/cjvyxov7x0sxr1clhccf42amm',
+    // style: 'mapbox://styles/rorschach07b/cjvyxgqqw0sre1co0ulwlnkf9',
+    zoom: 4.75,
+    center: [122.5, 12.5],
+     maxBounds: [[95, -15], [150, 40]]
+});
+
+map.on('load', function () {
+    var candidate = document.getElementById('candidate').value
+    var data = document.getElementById('data').value
+    var field = candidate + ' - ' + data
+
+    map.addSource(
+        'provinces+ncr_results', {
+            type: 'geojson',
+            data: 'static/data/elections2019-partylists-provinces+ncr.geojson'
+        }
+    );
+
+    map.addLayer({
+        'id': 'provinces+ncr',
+        'type': 'fill',
+        'source': 'provinces+ncr_results',
+        'layout': {},
+        'paint': {
+            'fill-color': styleField(),
+            'fill-outline-color': '#000000',
+        },
+    });
+});
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+map.on('click', 'provinces+ncr', function (e) {
+    var candidate = document.getElementById('candidate').value
+    var coordinates = e.lngLat;
+    var coc = e.features[0].properties['COC_2019'];
+    var votes = e.features[0].properties[candidate + ' - VOTES'];
+    var percentage = e.features[0].properties[candidate + ' - PERCENTAGE'];
+    var percent_total_votes = e.features[0].properties[candidate + ' - PERCENT TOTAL VOTES'];
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    var popup_html ='<h6>' + coc + '</h6>'
+                 + '<strong><h6>' + candidate + '</h6></strong><hr style="margin: 0.5em 0em">'
+                 + '<p style="font-size: 15px"><strong>VOTES: </strong>' + votes
+                 + '<br><strong>PERCENTAGE: </strong>' + percentage
+                 + '<br><strong>PERCENT OF TOTAL VOTES: </strong>' + percent_total_votes + '</p>'
+
+    new mapboxgl.Popup({closeButton:''})
+        .setLngLat(coordinates)
+        .setHTML(popup_html)
+        .addTo(map);
+});
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'provinces+ncr', function () {
+map.getCanvas().style.cursor = 'pointer';
+});
+
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'provinces+ncr', function () {
+map.getCanvas().style.cursor = '';
+});
---
+++ static/js/senators-compare.js
@@ -0,0 +1,110 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoicm9yc2NoYWNoMDdiIiwiYSI6ImNqYjh1NDRwYjBiaGEzM251bWF0a3c1cnAifQ.FEHNJbpxcs_UxuRhKbGF6A';
+
+document.getElementById('candidate1').onchange = function() {updateMap()};
+document.getElementById('candidate2').onchange = function() {updateMap()};
+
+function updateMap() {
+    map.setPaintProperty('provinces+ncr',
+        'fill-color',
+            styleField(),
+     );
+}
+
+function styleField() {
+    var candidate1 = document.getElementById('candidate1').value
+    var candidate2 = document.getElementById('candidate2').value
+
+    return [
+    'case',
+        ['all', ['<=', ['get', candidate1 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate2 + ' - PERCENTAGE'], 2.0]], '#e8e8e8',
+        ['all', ['>', ['get', candidate1 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate1 + ' - PERCENTAGE'], 5.0], ['<=', ['get', candidate2 + ' - PERCENTAGE'], 2.0]], '#ace4e4',
+        ['all', ['>', ['get', candidate1 + ' - PERCENTAGE'], 5.0], ['<=', ['get', candidate2 + ' - PERCENTAGE'], 2.0]], '#5ac8c8',
+        ['all', ['<=', ['get', candidate1 + ' - PERCENTAGE'], 2.0], ['>', ['get', candidate2 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate2 + ' - PERCENTAGE'], 5.0]], '#dfb0d6',
+        ['all', ['<=', ['get', candidate1 + ' - PERCENTAGE'], 2.0], ['>', ['get', candidate2 + ' - PERCENTAGE'], 5.0]], '#be64ac',
+        ['all', ['>', ['get', candidate1 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate1 + ' - PERCENTAGE'], 5.0], ['>', ['get', candidate2 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate2 + ' - PERCENTAGE'], 5.0]], '#a5add3',
+        ['all', ['>', ['get', candidate1 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate1 + ' - PERCENTAGE'], 5.0], ['>', ['get', candidate2 + ' - PERCENTAGE'], 5.0]], '#8c62aa',
+        ['all', ['>', ['get', candidate1 + ' - PERCENTAGE'], 5.0], ['>', ['get', candidate2 + ' - PERCENTAGE'], 2.0], ['<=', ['get', candidate2 + ' - PERCENTAGE'], 5.0]], '#5698b9',
+        ['all', ['>', ['get', candidate1 + ' - PERCENTAGE'], 5.0], ['>', ['get', candidate2 + ' - PERCENTAGE'], 5.0]], '#3b4994',
+        '#ffffff'
+    ]
+}
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/rorschach07b/cjvyxov7x0sxr1clhccf42amm',
+    // style: 'mapbox://styles/rorschach07b/cjvyxgqqw0sre1co0ulwlnkf9',
+    zoom: 4.75,
+    center: [122.5, 12.5],
+     maxBounds: [[95, -15], [150, 40]]
+});
+
+map.on('load', function () {
+    var candidate1 = document.getElementById('candidate1').value
+    var candidate2 = document.getElementById('candidate2').value
+
+    map.addSource(
+        'provinces+ncr_results', {
+            type: 'geojson',
+            data: 'static/data/elections2019-provinces+ncr.geojson'
+        }
+    );
+
+    map.addLayer({
+        'id': 'provinces+ncr',
+        'type': 'fill',
+        'source': 'provinces+ncr_results',
+        'layout': {},
+        'paint': {
+            'fill-color': styleField(),
+            'fill-outline-color': '#000000',
+        },
+    });
+});
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+map.on('click', 'provinces+ncr', function (e) {
+    var candidate1 = document.getElementById('candidate1').value
+    var candidate2 = document.getElementById('candidate2').value
+    var coordinates = e.lngLat;
+    var coc = e.features[0].properties['COC_2019'];
+    var votes1 = e.features[0].properties[candidate1 + ' - VOTES'];
+    var percentage1 = e.features[0].properties[candidate1 + ' - PERCENTAGE'];
+    var percent_total_votes1 = e.features[0].properties[candidate1 + ' - PERCENT TOTAL VOTES'];
+    var votes2 = e.features[0].properties[candidate2 + ' - VOTES'];
+    var percentage2 = e.features[0].properties[candidate2 + ' - PERCENTAGE'];
+    var percent_total_votes2 = e.features[0].properties[candidate2 + ' - PERCENT TOTAL VOTES'];
+
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+
+    // var popup_html ='<h6>' + coc + '</h6>'
+    //              + '<strong><h6>' + candidate + '</h6></strong><hr style="margin: 0.5em 0em">'
+    //              + '<p style="font-size: 15px"><strong>VOTES: </strong>' + votes
+    //              + '<br><strong>PERCENTAGE: </strong>' + percentage
+    //              + '<br><strong>PERCENT OF TOTAL VOTES: </strong>' + percent_total_votes + '</p>'
+
+    var popup_html = '<h6>' + coc + '</h6>' +
+                     '<table>' + '<tr><th style="padding: 4px;">Candidate</th><th style="padding: 4px;">Votes</th><th style="padding: 4px;">%</th></tr>' +
+                     '<tr><td style="padding: 4px;">' + candidate1 + '</td>' + '<td style="padding: 4px;">' + votes1 + '</td>' + '<td style="padding: 4px;">' + percentage1 + '</td></tr>' +
+                     '<tr><td style="padding: 4px;">' + candidate2 + '</td>' + '<td style="padding: 4px;">' + votes2 + '</td>' + '<td style="padding: 4px;">' + percentage2 + '</td></tr>' + '</table>'
+
+    new mapboxgl.Popup({closeButton:''})
+        .setLngLat(coordinates)
+        .setHTML(popup_html)
+        .addTo(map);
+});
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'provinces+ncr', function () {
+map.getCanvas().style.cursor = 'pointer';
+});
+
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'provinces+ncr', function () {
+map.getCanvas().style.cursor = '';
+});
